cc157bfd2c29a08de62ae95d603dd70e5b000540
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2024-10-24 16:01:29 +0700

    Add files via upload

---
+++ buku.js
@@ -0,0 +1,85 @@
+const Buku = require('../model/buku');
+
+const createBuku = (req,res) => {
+    const buku = new Buku({
+        judul: req.body.judul,
+        penulis: req.body.penulis,
+        genre: req.body.genre
+    });
+
+    console.log(buku);
+    buku.save()
+    .then((createdBuku)=>{
+        res.status(201).json({
+            message : "Data berhasil disimpan",
+            bookId : createdBuku._id
+        });
+    })
+    .catch((err)=>{
+        // console.log(err);
+        res.status(500).json({
+            message : "Internal Server Error",
+            // error : err
+        });
+    });
+};
+
+const readBuku = (req,res) => {
+    Buku.find()
+    .then((documents)=>{
+        res.status(200).json({
+            message : "Get Data Buku",
+            bukus : documents
+        });
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message : "Internal Server Error",
+            error : err
+        });
+    });
+};
+
+const deleteBuku = (req, res) => {
+    Buku.deleteOne({_id: req.params.id})
+    .then((result)=>{
+        res.status(200).json({
+            message: "Buku berhasil dihapus",
+            result: result
+        });
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: err
+        });
+    });
+};
+
+const updateBuku = (req, res) => {
+   
+    const buku = new Buku({
+       _id : req.params.id,
+       judul : req.body.judul,
+       penulis : req.body.penulis,
+       genre : req.body.genre
+   });
+
+
+   Buku.updateOne({_id : req.params.id}, buku)
+   .then((hasil)=>{
+       res.status(200).json({
+           message : "Update Berhasil",
+           result : hasil    
+       });
+   })
+   .catch((err)=>{
+       res.status(500).json({
+           message : "Internal server error"
+       });
+   });
+
+
+};
+
+module.exports = {createBuku, readBuku, deleteBuku, updateBuku};
